0633a5250421d97058a4c770c5a735ed7a1ef3b5
Nam <namngoc.972k@gmail.com>
2020-06-09 13:25:14 +0700

    css

--- TSB/Scripts/button.js
+++ TSB/Scripts/button.js
@@ -1,23 +1,4 @@
-﻿//Lấy các button mô tả nội dung
-var acc = document.getElementsByClassName("btn");
-var i;
-//lặp qua các button để gán sự kiện
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function () {
-        /* Thêm/xóa class active để đánh dấu các button đã được click */
-        this.classList.toggle("active");
-
-        /* Hiển thị hoặc ẩn nội dung khi button được click */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    }
-}
-
-
+﻿
 
 function easeOutExpo(x) {
     return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
